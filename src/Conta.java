b3adb76e9c00e29034f53c71a8d36aca8ce1dac1
TulioHRC <tulio.prezotti@gmail.com>
2025-11-25 23:44:07 -0300

    refactor: valor -> saldo

--- src/Conta.java
+++ src/Conta.java
@@ -4,19 +4,19 @@ import java.util.List;
 public class Conta {
 
     private int numConta;
+    
     private Cliente cliente;
     private Agencia agencia;
 
-    // TODO(#2) REFATORAR: Esse nome não é o ideal para representar o saldo da conta
-    private double valor;
+    private double saldo;
 
     private List<Operacao> operacoes;
 
-    public Conta(Cliente cliente, Agencia agencia, int numConta, double valor) {
+    public Conta(Cliente cliente, Agencia agencia, int numConta, double saldo) {
         this.cliente = cliente;
         this.agencia = agencia;
         this.numConta = numConta;
-        this.valor = valor;
+        this.saldo = saldo;
 
         this.operacoes = new ArrayList<>();
     }
@@ -26,19 +26,19 @@ public class Conta {
     }
 
     // TODO(#3) REFATORAR: Muita responsabilidade para o mesmo método
-    public void realizarOperacao(char tipo, int valor) {
-        Operacao op = new Operacao(tipo, valor);
+    public void realizarOperacao(char tipo, int saldo) {
+        Operacao op = new Operacao(tipo, saldo);
         this.operacoes.add(op);
 
         if (tipo == 'd')
-            this.valor += valor;
+            this.saldo += saldo;
         else if(tipo == 's')
-            this.valor -= valor;
+            this.saldo -= saldo;
     }
 
     public String toString() {
         String dadosConta = String.format("Ag.: %d\nConta: %d\nGerente: %s\nSaldo: %.2f",
-                this.agencia.getNumAgencia(), this.numConta, this.agencia.getGerente(), this.valor);
+                this.agencia.getNumAgencia(), this.numConta, this.agencia.getGerente(), this.saldo);
 
         // TODO(#5) REFATORAR: Essa operação não deveria estar sendo realizada neste método
         String dadosExtrato = "";
